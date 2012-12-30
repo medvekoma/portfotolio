35544fb4827fd717c6b28ec42a5cd317f21c970c
Attila Szucs <attila.szucs@gmail.com>
2012-12-30 21:36:43 +0100

    start json api

---
+++ master/FlickrWrapper/FlickrApi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace FlickrWrapper
+{
+    public class FlickrApi
+    {
+        private readonly string _apiKey;
+
+        public FlickrApi(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public string PeopleGetPublicPhotos(string userId, 
+            string safeSearch = null, string extras = null, 
+            int? perPage = null, int? page = null)
+        {
+            string url = "http://api.flickr.com/services/rest?method=flickr.people.getPublicPhotos&";
+            var parameters = new Dictionary<string, string>
+                {
+                    {"format", "json"}, 
+                    {"nojsoncallback", "1"},
+                    {"api_key", _apiKey}, 
+                    {"user_id", userId}
+                };
+            if (safeSearch != null)
+                parameters.Add("safe_search", safeSearch);
+            if (extras != null)
+                parameters.Add("extras", extras);
+            if (perPage != null)
+                parameters.Add("per_page", perPage.Value.ToString(CultureInfo.InvariantCulture));
+            if (page != null)
+                parameters.Add("page", page.Value.ToString(CultureInfo.InvariantCulture));
+
+            var parameterArray = parameters.Select(item => item.Key + "=" + item.Value).ToArray();
+            url += string.Join("&", parameterArray);
+
+            using (var webClient = new WebClient())
+            using (var stream = webClient.OpenRead(url))
+            {
+                if (stream == null)
+                    return null;
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ master/FlickrWrapperTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using FlickrWrapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlickrWrapperTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var flickrApi = new FlickrApi("");
+            var response = flickrApi.PeopleGetPublicPhotos("27725019@N00");
+
+            Console.WriteLine(response);
+        }
+    }
+}
