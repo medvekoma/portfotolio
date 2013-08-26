585bdf26f8c1f918af323e48d88f587eb53174ad
Attila Szucs <attila.szucs@gmail.com>
2013-08-26 17:10:51 +0200

    rename bundles

--- master/Site4/App_Start/BundleConfig.cs
+++ master/Site4/App_Start/BundleConfig.cs
@@ -2,19 +2,18 @@
 
 namespace Portfotolio.Site4
 {
-    public class BundleConfig
+	// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+	public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
-
-        const string JqueryCdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.3.min.js"; //add link to jquery on the CDN
+        const string JqueryCdnPath = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.3.min.js";
 
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
-            bundles.Add(new ScriptBundle("~/Scripts/js").Include("~/Scripts/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/Paging/js").Include("~/Scripts/Paging/*.js"));
-            bundles.UseCdn = true;   //enable CDN support
-            bundles.Add(new ScriptBundle("~/Scripts/jquery", JqueryCdnPath).Include("~/Scripts/jquery/jquery-{version}.js"));
+			bundles.UseCdn = true;
+			bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/*.css"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include("~/Scripts/*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/paging").Include("~/Scripts/Paging/*.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery", JqueryCdnPath).Include("~/Scripts/jquery/jquery-{version}.js"));
         }
     }
 }
