00f97ebf730853a87aa13ff110840901b6915aeb
eserkansozer <Serkan_Sozer@EPGBLONW0031.budapest.epam.com>
2013-08-27 16:11:31 +0100

    Revert "Replacing LoggerFactory with LoggerProvider"
    This reverts commit 8a5b81768b34324b7b9f97f825f5bd9b82462baf.

---
+++ master/Services/Logging/LoggerFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfotolio.Services.Logging
+{
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger GetLogger(string name)
+        {
+            return new NLogLoggerFactory().GetLogger(name);
+        }
+    }
+}
