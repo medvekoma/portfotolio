86f6d372cd4f65978f96ec9fe923d7f48ad4f8a4
eserkansozer <Serkan_Sozer@EPGBLONW0031.budapest.epam.com>
2013-08-29 12:18:53 +0100

    Scroll History

--- master/Site4/Scripts/Paging/portfotolio.PagingView.js
+++ master/Site4/Scripts/Paging/portfotolio.PagingView.js
@@ -3,6 +3,7 @@ if (typeof Medvekoma.Portfotolio == "undefined") Medvekoma.Portfotolio = {};
 
 var delay = 100;        // page load delay in milliseconds
 var scrollMargin = 350; // bottom pixels before automatic paging
+var historicalScrollPosition = 0;
 
 $(document).ready(function () {
     Medvekoma.Portfotolio.InitializeLoading();
@@ -11,17 +12,37 @@ $(document).ready(function () {
 Medvekoma.Portfotolio.InitializeLoading = function () {
     $('#nextPageLink').hide();
     $('#nextPageLoading').show();
-    var requestedScrollPosition = $(window).scrollTop();
+    
+    var requestedScrollPosition = Medvekoma.Portfotolio.CalculateTargetScollPosition();
     Medvekoma.Portfotolio.RestoreScrollPosition(requestedScrollPosition);
     Medvekoma.Portfotolio.ShowNextPageIfNeeded();
     Medvekoma.Portfotolio.SubscribeToScrollEvent();
 };
 
-Medvekoma.Portfotolio.RestoreScrollPosition = function (scrollPosition) {
-    while (scrollPosition > $(document).height()) {
-        Medvekoma.Portfotolio.ShowNextPage();
+Medvekoma.Portfotolio.CalculateTargetScollPosition = function() {
+
+    var targetScrollPosition = $(window).scrollTop();//default case within the page
+
+    if (Medvekoma.Portfotolio.GetUrlParameters()["scroll"] > targetScrollPosition)//use the one from URL when coming back from Flickr(first partialpage load)
+    {
+        targetScrollPosition = parseInt(Medvekoma.Portfotolio.GetUrlParameters()["scroll"]);
+        if(historicalScrollPosition == 0)
+            historicalScrollPosition = targetScrollPosition;
     }
-    $(window).scrollTop(scrollPosition);
+
+    if (historicalScrollPosition > targetScrollPosition)//use the original one when was requested coming back from Flickr for the first time
+        targetScrollPosition = historicalScrollPosition;
+
+    return targetScrollPosition;
+};
+
+Medvekoma.Portfotolio.RestoreScrollPosition = function (scrollPosition) {
+    if (scrollPosition > $(document).height()) {
+        //window.setTimeout(function() {
+            Medvekoma.Portfotolio.ShowNextPageIfNeeded();
+        //}, delay);
+        $(window).scrollTop(scrollPosition);
+    }
 };
 
 Medvekoma.Portfotolio.SubscribeToScrollEvent = function () {
@@ -32,7 +53,7 @@ Medvekoma.Portfotolio.SubscribeToScrollEvent = function () {
 
 Medvekoma.Portfotolio.ShowNextPageIfNeeded = function () {
     var invisiblePixels = $(document).height() - $(window).scrollTop() - $(window).height();
-    if (invisiblePixels < scrollMargin) {
+    if (invisiblePixels < scrollMargin || Medvekoma.Portfotolio.CalculateTargetScollPosition() > $(document).height()){
         $(window).unbind('scroll');
         window.setTimeout(Medvekoma.Portfotolio.ShowNextPage, delay);
     }
@@ -48,8 +69,27 @@ Medvekoma.Portfotolio.ShowNextPage = function () {
             success: function (result) {
                 nextPageDiv.html('').replaceWith(result);
                 Medvekoma.Portfotolio.InitializeLoading();
+                history.pushState(null, null, "?scroll=" + $(document).height());
+                window.addEventListener("popstate", function () {
+                    var urlParams = Medvekoma.Portfotolio.GetUrlParameters();
+                    Medvekoma.Portfotolio.RestoreScrollPosition(urlParams["scroll"]);
+                });
             }
         };
         $.ajax(options);
     }
+};
+
+
+Medvekoma.Portfotolio.GetUrlParameters = function() {
+    var match,
+        pl = /\+/g, // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function(s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query = window.location.search.substring(1);
+
+    var urlParams = {};
+    while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+    return urlParams;
 };
