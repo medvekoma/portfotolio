d8efe71fc109147ae8ee3bdebf20e04e19ea0a9c
eserkansozer <Serkan_Sozer@EPGBLONW0031.budapest.epam.com>
2013-09-09 15:23:56 +0100

    Revert "Restricting the routes only for serkansozer"
    This reverts commit 7d6bab2538d3e743045a0d4577733be4860dcd42.

--- master/Site4/App_Start/RouteConfig.cs
+++ master/Site4/App_Start/RouteConfig.cs
@@ -11,38 +11,31 @@ namespace Portfotolio.Site4
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("{*url}", new { url = @"^.+\.[a-zA-Z]{2,4}$" });
 
-            //routes.MapRoute(
-            //    name: "home",
-            //    url: "",
-            //    defaults: new { controller = "photo", action = "promotion" }
-            //    );
-
             routes.MapRoute(
                 name: "home",
                 url: "",
-                defaults: new { controller = "photo", action = "photos", id = "serkansozer" }
+                defaults: new { controller = "photo", action = "promotion" }
                 );
 
-            //routes.MapRoute(
-            //    name: "group",
-            //    url: "group/{id}",
-            //    defaults: new { controller = "photo", action = "group", id = UrlParameter.Optional },
-            //    constraints: new { id = "^[0-9].*$"}
-            //    );
+            routes.MapRoute(
+                name: "group",
+                url: "group/{id}",
+                defaults: new { controller = "photo", action = "group", id = UrlParameter.Optional },
+                constraints: new { id = "^[0-9].*$"}
+                );
 
             routes.MapRoute(
                 name: "photo",
                 url: "{id}/{action}/{secondaryId}", 
                 defaults: new { controller = "photo", action = "photos", secondaryId = UrlParameter.Optional },
-                constraints: new { id = "serkansozer" }
-
+                constraints: new { id = "^[^-].*$" }
                 );
 
-            //routes.MapRoute(
-            //    name: "generic",
-            //    url: "-{controller}/{action}/{id}",
-            //    defaults: new { action = "show", id = UrlParameter.Optional }
-            //    );
+            routes.MapRoute(
+                name: "generic",
+                url: "-{controller}/{action}/{id}",
+                defaults: new { action = "show", id = UrlParameter.Optional }
+                );
 
             routes.MapRoute(
                 name: "catch-all",
